67f6f4798c0742f719c03a011d5c87a4d49fa871
Greg Oleksiak <greg.oleksiak@gmail.com>
2014-08-30 15:11:47 -0400

    Update opennlp libs

--- lib/open-nlp/bindings.rb
+++ lib/open-nlp/bindings.rb
@@ -20,9 +20,9 @@ module OpenNLP::Bindings
   # Default JARs to load.
   self.default_jars = [
     'jwnl-1.3.3.jar',
-    'opennlp-tools-1.5.2-incubating.jar',
-    'opennlp-maxent-3.0.2-incubating.jar',
-    'opennlp-uima-1.5.2-incubating.jar'
+    'opennlp-tools-1.5.3-incubating.jar',
+    'opennlp-maxent-3.0.3-incubating.jar',
+    'opennlp-uima-1.5.3-incubating.jar'
   ]
 
   # Default namespace.
